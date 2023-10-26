95602057fcefc6499dcfa1fe13c2e3017d24cfe5
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-10-26 12:21:45 +0100

    Update app.js

--- 01-intro-typescript/app.js
+++ 01-intro-typescript/app.js
@@ -1,5 +1,11 @@
-let a = 5;
-a = 'hello';
-a = {name: 'Pesho', age: 21};
+// let a = 5;
+// a = 'hello';
+// a = {name: 'Pesho', age: 21};
 
 console.log(a);
+
+function calc(a) {
+    return a * a;
+}
+
+calc(5);
