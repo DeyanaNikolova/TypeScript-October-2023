72b71c338a2c6bddd82b52f17d38c37def8c5099
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-10-26 12:46:22 +0100

    Add example of class

--- 01-intro-typescript/app.js
+++ 01-intro-typescript/app.js
@@ -1,11 +1,9 @@
-// let a = 5;
-// a = 'hello';
-// a = {name: 'Pesho', age: 21};
-
-console.log(a);
-
-function calc(a) {
-    return a * a;
-}
-
-calc(5);
+var Person = /** @class */ (function () {
+    function Person(fName) {
+        this.firstName = fName;
+    }
+    Person.prototype.greeting = function () {
+        return "Hello ".concat(this.firstName, "!");
+    };
+    return Person;
+}());
