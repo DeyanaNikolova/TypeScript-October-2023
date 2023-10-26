97a46c7e5d0b70a175819dc00a28ff369e1ff69d
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-10-26 13:03:08 +0100

    Remove files

--- 01-intro-typescript/app.js
+++
@@ -1,9 +0,0 @@
-var Person = /** @class */ (function () {
-    function Person(fName) {
-        this.firstName = fName;
-    }
-    Person.prototype.greeting = function () {
-        return "Hello ".concat(this.firstName, "!");
-    };
-    return Person;
-}());
