3eaac9d26456457f3b9fd9248de2fd35f958c394
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-10-26 14:39:35 +0100

    Add type number example

--- 01-intro-typescript/src/app.ts
+++ 01-intro-typescript/src/app.ts
@@ -1,7 +1,13 @@
-let greetingMessage = "Hello";
+let greetingMessage: string = "Hello";
 greetingMessage = "Ok";
 greetingMessage = `Ok (updated) ${123}`;
 
-console.log(greetingMessage);
+console.log('string', greetingMessage);
+
+let numberOfHouses: number = 4;
+numberOfHouses = 10103945495;
+numberOfHouses = 7e3;
+console.log('number', numberOfHouses);
+
 
 
