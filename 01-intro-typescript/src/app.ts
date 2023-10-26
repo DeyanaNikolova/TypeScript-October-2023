5fbdcdd81e49675a9f60fb21f8bf5a698896d1a8
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-10-26 14:49:08 +0100

    Add example for type boolean

--- 01-intro-typescript/src/app.ts
+++ 01-intro-typescript/src/app.ts
@@ -1,13 +1,21 @@
 let greetingMessage: string = "Hello";
 greetingMessage = "Ok";
 greetingMessage = `Ok (updated) ${123}`;
-
-console.log('string', greetingMessage);
+// console.log('string', greetingMessage);
 
 let numberOfHouses: number = 4;
 numberOfHouses = 10103945495;
 numberOfHouses = 7e3;
-console.log('number', numberOfHouses);
+// console.log('number', numberOfHouses);
+
+let isDog: boolean = true;
+isDog = false;
+isDog = 5 > 2;
+isDog = [1, 2, 3, 4].includes(100);
+isDog = [1, 2, 3, 4].some(e => e === 1);
+console.log('boolean', isDog);
+
+
 
 
 
