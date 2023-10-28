b620bc93a4331659780c07103a937fdd8dca06bb
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-10-28 11:32:42 +0100

    Add example for null and undefined types

--- 01-intro-typescript/src/app.ts
+++ 01-intro-typescript/src/app.ts
@@ -1,27 +1,44 @@
+// Type String
+
 let greetingMessage: string = "Hello";
 greetingMessage = "Ok";
 greetingMessage = `Ok (updated) ${123}`;
 // console.log('string', greetingMessage);
+
+
+// Type Number
 
 let numberOfHouses: number = 4;
 numberOfHouses = 10103945495;
 numberOfHouses = 7e3;
 // console.log('number', numberOfHouses);
 
+
+//  Type Boolean
+
 let isDog: boolean = true;
 isDog = false;
 isDog = 5 > 2;
 isDog = [1, 2, 3, 4].includes(100);
 isDog = [1, 2, 3, 4].some(e => e === 1);
-console.log('boolean', isDog);
+// console.log('boolean', isDog);
 
+//  Type Symbol
 
 let uniqueSymbol: Symbol = Symbol('myUniqueSymbol');
 let anotherUnoqueSymbol: Symbol = Symbol('myUniqueSymbol');
 const areEqual = uniqueSymbol === anotherUnoqueSymbol;
 
-console.log('symbol', uniqueSymbol);
-console.log(areEqual);
+// console.log('symbol', uniqueSymbol);
+// console.log('areEqual', areEqual);
 
+
+// Type null and undefined
+
+let person: undefined;
+console.log(typeof person);
+
+let dogs: null = null;
+console.log('null: ', dogs);
 
 
