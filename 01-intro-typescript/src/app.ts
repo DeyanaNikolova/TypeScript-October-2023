127098629b6f8a11f952d2910f0227863d980a8c
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-10-28 11:57:39 +0100

    Add example for type array

--- 01-intro-typescript/src/app.ts
+++ 01-intro-typescript/src/app.ts
@@ -1,5 +1,4 @@
 // Type String
-
 let greetingMessage: string = "Hello";
 greetingMessage = "Ok";
 greetingMessage = `Ok (updated) ${123}`;
@@ -7,7 +6,6 @@ greetingMessage = `Ok (updated) ${123}`;
 
 
 // Type Number
-
 let numberOfHouses: number = 4;
 numberOfHouses = 10103945495;
 numberOfHouses = 7e3;
@@ -15,7 +13,6 @@ numberOfHouses = 7e3;
 
 
 //  Type Boolean
-
 let isDog: boolean = true;
 isDog = false;
 isDog = 5 > 2;
@@ -24,7 +21,6 @@ isDog = [1, 2, 3, 4].some(e => e === 1);
 // console.log('boolean', isDog);
 
 //  Type Symbol
-
 let uniqueSymbol: Symbol = Symbol('myUniqueSymbol');
 let anotherUnoqueSymbol: Symbol = Symbol('myUniqueSymbol');
 const areEqual = uniqueSymbol === anotherUnoqueSymbol;
@@ -34,11 +30,31 @@ const areEqual = uniqueSymbol === anotherUnoqueSymbol;
 
 
 // Type null and undefined
-
 let person: undefined;
-console.log(typeof person);
+// console.log(typeof person);
 
 let dogs: null = null;
-console.log('null: ', dogs);
+// console.log('null: ', dogs);
+
+//  Type Array
+const arrOfNumbers: number[] = [1, 2, 3, 4];
+// console.log('array of numbers', arrOfNumbers);
+
+const arrOfStrings: string[] = ['Pesho', 'Gosho', 'Stamat'];
+// console.log('array of strings', arrOfStrings);
+
+const arrOfBools: boolean[] = [true, false, true];
+// console.log('array of booleans', arrOfBools);
+
+
+// Array of objects
+type Person = {
+    name: string;
+    age: number;
+};
+
+const arrOfObj: Person[] = [{name: 'Pesho', age: 25}, {name: 'Ivan', age: 15}];
+console.log('array of objects', arrOfObj);
+
 
 
