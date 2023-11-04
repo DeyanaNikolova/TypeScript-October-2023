5f58264913987a73887ba554ee65d0832e9ed78b
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-11-04 15:07:28 +0000

    Add user interface

--- 03-interfaces-and-generics/src/app.ts
+++ 03-interfaces-and-generics/src/app.ts
@@ -1,1 +1,19 @@
-console.log(1+2);
+// Contract
+interface UserDetails {
+  firstName: string;
+  lastName: string;
+  email: string;
+  tellDetais: () => void;
+}
+
+const user: UserDetails = {
+  firstName: "Pesho",
+  lastName: "Ivanov",
+  email: "pesho@gmail.com",
+  tellDetais: function () {
+    console.log(`User Details: ${this.firstName} ${this.lastName} with email: ${this.email}`);
+    
+  },
+};
+
+user.tellDetais();
