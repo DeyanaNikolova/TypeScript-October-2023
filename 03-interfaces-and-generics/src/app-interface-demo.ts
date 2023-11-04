87ec9d173c6daba3d3cb82de02e789e2f2cc20c7
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-11-04 16:38:44 +0000

    Add example of function type

--- 03-interfaces-and-generics/src/app-interface-demo.ts
+++ 03-interfaces-and-generics/src/app-interface-demo.ts
@@ -7,64 +7,74 @@ interface UserDetails {
 }
 
 interface Human {
-    legsCount: number;
-    skinColor: string;
-   
-  }
+  legsCount: number;
+  skinColor: string;
+}
 
-  // Can extebds other interfaces
-  interface PersonDetails extends UserDetails, Human {
-    fullName: string;
-  }
+// Can extebds other interfaces
+interface PersonDetails extends UserDetails, Human {
+  fullName: string;
+}
 
 // Can be implemented by a class
 class Person implements PersonDetails {
-    firstName: string;
-    lastName: string;
-    legsCount: number;
-    skinColor: string;
-    fullName: string;
-  
-    constructor(fName: string, lName: string, legsCount: number, skinColor: string) {
-      this.firstName = fName;
-      this.lastName = lName;
-      this.legsCount = legsCount;
-      this.skinColor = skinColor;
-      
-      this.fullName = this.firstName + ' ' + this.lastName;
-    }
-  }
+  firstName: string;
+  lastName: string;
+  legsCount: number;
+  skinColor: string;
+  fullName: string;
 
+  constructor(
+    fName: string,
+    lName: string,
+    legsCount: number,
+    skinColor: string
+  ) {
+    this.firstName = fName;
+    this.lastName = lName;
+    this.legsCount = legsCount;
+    this.skinColor = skinColor;
+
+    this.fullName = this.firstName + " " + this.lastName;
+  }
+}
 
 //   Can be used as a type of a variable
 const user: UserDetails = {
   firstName: "Pesho",
   lastName: "Ivanov",
-//   email: "pesho@gmail.com",
-//   tellDetais: function () {
-//     console.log(`User Details: ${this.firstName} ${this.lastName} with email: ${this.email}`);
-//   },
+  //   email: "pesho@gmail.com",
+  //   tellDetais: function () {
+  //     console.log(`User Details: ${this.firstName} ${this.lastName} with email: ${this.email}`);
+  //   },
 };
 
 // user.tellDetais();
 
-
 // Can be used as a return type
 function getUsers(): UserDetails[] {
-    return [
-        {
-        firstName: "Pesho",
-        lastName: "Ivanov",
-      },
-      {
-        firstName: "Maria",
-        lastName: "Petrova",
-      },
-      {
-        firstName: "Mitko",
-        lastName: "Mitkov",
-      },
-    ]
+  return [
+    {
+      firstName: "Pesho",
+      lastName: "Ivanov",
+    },
+    {
+      firstName: "Maria",
+      lastName: "Petrova",
+    },
+    {
+      firstName: "Mitko",
+      lastName: "Mitkov",
+    },
+  ];
 }
 
 const users = getUsers();
+
+interface GetNameFunction {
+  (param1: number, param2: string): string;
+}
+
+const getName: GetNameFunction = (param1: Number, param2: string) => {
+  return "";
+};
