dad8b63b49447cd56517488d060d70e79e0ec2bb
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-11-04 15:35:24 +0000

    Extend and implement interfaces

--- 03-interfaces-and-generics/src/app.ts
+++ 03-interfaces-and-generics/src/app.ts
@@ -2,18 +2,67 @@
 interface UserDetails {
   firstName: string;
   lastName: string;
-  email: string;
-  tellDetais: () => void;
+  email?: string;
+  tellDetais?: () => void;
 }
 
+interface Human {
+    legsCount: number;
+    skinColor: string;
+   
+  }
+
+  interface PersonDetails extends UserDetails, Human {
+    fullName: string;
+  }
+
+// Can be implemented by a class
+class Person implements PersonDetails {
+    firstName: string;
+    lastName: string;
+    legsCount: number;
+    skinColor: string;
+    fullName: string;
+  
+    constructor(fName: string, lName: string, legsCount: number, skinColor: string) {
+      this.firstName = fName;
+      this.lastName = lName;
+      this.legsCount = legsCount;
+      this.skinColor = skinColor;
+      this.fullName = this.firstName + ' ' + this.lastName;
+    }
+  }
+
+
+//   Can be used as a type of a variable
 const user: UserDetails = {
   firstName: "Pesho",
   lastName: "Ivanov",
-  email: "pesho@gmail.com",
-  tellDetais: function () {
-    console.log(`User Details: ${this.firstName} ${this.lastName} with email: ${this.email}`);
-    
-  },
+//   email: "pesho@gmail.com",
+//   tellDetais: function () {
+//     console.log(`User Details: ${this.firstName} ${this.lastName} with email: ${this.email}`);
+//   },
 };
 
-user.tellDetais();
+// user.tellDetais();
+
+
+// Can be used as a return type
+function getUsers(): UserDetails[] {
+    return [
+        {
+        firstName: "Pesho",
+        lastName: "Ivanov",
+      },
+      {
+        firstName: "Maria",
+        lastName: "Petrova",
+      },
+      {
+        firstName: "Mitko",
+        lastName: "Mitkov",
+      },
+    ]
+}
+
+const users = getUsers();
