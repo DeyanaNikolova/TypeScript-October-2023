b564ebeaeb57a80b0b17b7626ef56b3ee84f658d
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-11-05 10:17:26 +0000

    Add example for generics

--- 03-interfaces-and-generics/src/generics-demo/app.ts
+++ 03-interfaces-and-generics/src/generics-demo/app.ts
@@ -14,47 +14,72 @@
 //   address: string;
 //   id: T;
 // };
-// const x = getTest<UserData<number>>({ 
-//     name: "Ivan", 
-//     age: 23, 
+// const x = getTest<UserData<number>>({
+//     name: "Ivan",
+//     age: 23,
 //     address: "Sofia, Bulgaria",
 //     id:12
 // });
 
-// const y = getTest<UserData<string>>({ 
-//     name: "Ivan", 
-//     age: 23, 
+// const y = getTest<UserData<string>>({
+//     name: "Ivan",
+//     age: 23,
 //     address: "Sofia, Bulgaria",
 //     id: 'asdasd'
 // });
 
-const generateAndAttachId = <T>(obj: T) => {
-    const objId = Math.random();
-return {...obj, id: 1};
+// Generic use case
+
+// const generateAndAttachId = <T>(obj: T) => {
+//     const objId = Math.random();
+// return {...obj, id: 1};
+// }
+
+// type PersonObjData = {
+//     name: String;
+//     age: Number;
+//     city: String;
+// }
+
+// const person: PersonObjData = {name: 'Mitko', age: 22, city: 'Lovech'};
+// const personWithId = generateAndAttachId<PersonObjData>(person);
+// console.log(personWithId);
+
+// type AnimalObjData = {
+//     legsCVount: number;
+//     furColor: string;
+//     name: string;
+// }
+
+// const animal: AnimalObjData = {
+//     legsCVount: 4,
+//     furColor: 'brown',
+//     name: 'Mishi',
+// }
+
+// const animalWithId = generateAndAttachId(animal);
+// console.log(animalWithId);
+
+interface DocumentObj<T> {
+  id: number;
+  name: string;
+  data: T;
 }
 
-type PersonObjData = {
-    name: String;
-    age: Number;
-    city: String;
+const doc1 = {
+  id: 1,
+  name: "flowers",
+  data: [{ name: "rosa" }, { name: "daffodil" }, { name: "orhid" }],
+};
+
+const doc2 = {
+    id: 2,
+    name: 'persons',
+    data: {name: 'Pesho', age: 12, address: 'Bulgaria'},
 }
 
-const person: PersonObjData = {name: 'Mitko', age: 22, city: 'Lovech'};
-const personWithId = generateAndAttachId<PersonObjData>(person);
-console.log(personWithId);
-
-type AnimalObjData = {
-    legsCVount: number;
-    furColor: string;
-    name: string;
+const doc3 = {
+    id: 3,
+    name: 'test',
+    data: 1,
 }
-
-const animal: AnimalObjData = {
-    legsCVount: 4,
-    furColor: 'brown',
-    name: 'Mishi',
-}
-
-const animalWithId = generateAndAttachId(animal);
-console.log(animalWithId);
-
