edfa885991da17293db56f543ab3a9efa2081cc2
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-11-05 09:54:48 +0000

    Add use case of generics

--- 03-interfaces-and-generics/src/generics-demo/app.ts
+++ 03-interfaces-and-generics/src/generics-demo/app.ts
@@ -1,27 +1,60 @@
-const getTest = <T>(paramId: T): T => {
-  return paramId;
-};
+// Genrics
 
-const id1 = <number>1;
-const id2 = getTest<string>("sfsgsd");
-const ids = getTest<number[]>([1, 2, 3]);
+// const getTest = <T>(paramId: T): T => {
+//   return paramId;
+// };
 
-type UserData<T> = {
-  name: string;
-  age: number;
-  address: string;
-  id: T;
-};
-const x = getTest<UserData<number>>({ 
-    name: "Ivan", 
-    age: 23, 
-    address: "Sofia, Bulgaria",
-    id:12
-});
+// const id1 = <number>1;
+// const id2 = getTest<string>("sfsgsd");
+// const ids = getTest<number[]>([1, 2, 3]);
 
-const y = getTest<UserData<string>>({ 
-    name: "Ivan", 
-    age: 23, 
-    address: "Sofia, Bulgaria",
-    id: 'asdasd'
-});
+// type UserData<T> = {
+//   name: string;
+//   age: number;
+//   address: string;
+//   id: T;
+// };
+// const x = getTest<UserData<number>>({ 
+//     name: "Ivan", 
+//     age: 23, 
+//     address: "Sofia, Bulgaria",
+//     id:12
+// });
+
+// const y = getTest<UserData<string>>({ 
+//     name: "Ivan", 
+//     age: 23, 
+//     address: "Sofia, Bulgaria",
+//     id: 'asdasd'
+// });
+
+const generateAndAttachId = <T>(obj: T) => {
+    const objId = Math.random();
+return {...obj, id: 1};
+}
+
+type PersonObjData = {
+    name: String;
+    age: Number;
+    city: String;
+}
+
+const person: PersonObjData = {name: 'Mitko', age: 22, city: 'Lovech'};
+const personWithId = generateAndAttachId<PersonObjData>(person);
+console.log(personWithId);
+
+type AnimalObjData = {
+    legsCVount: number;
+    furColor: string;
+    name: string;
+}
+
+const animal: AnimalObjData = {
+    legsCVount: 4,
+    furColor: 'brown',
+    name: 'Mishi',
+}
+
+const animalWithId = generateAndAttachId(animal);
+console.log(animalWithId);
+
