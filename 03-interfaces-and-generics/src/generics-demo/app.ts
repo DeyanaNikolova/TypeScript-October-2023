836a58bbd3b68170616dd29ba3f9180007fa6ccd
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-11-05 09:35:18 +0000

    Add generics type example

--- 03-interfaces-and-generics/src/generics-demo/app.ts
+++ 03-interfaces-and-generics/src/generics-demo/app.ts
@@ -1,1 +1,27 @@
-console.log('test');
+const getTest = <T>(paramId: T): T => {
+  return paramId;
+};
+
+const id1 = <number>1;
+const id2 = getTest<string>("sfsgsd");
+const ids = getTest<number[]>([1, 2, 3]);
+
+type UserData<T> = {
+  name: string;
+  age: number;
+  address: string;
+  id: T;
+};
+const x = getTest<UserData<number>>({ 
+    name: "Ivan", 
+    age: 23, 
+    address: "Sofia, Bulgaria",
+    id:12
+});
+
+const y = getTest<UserData<string>>({ 
+    name: "Ivan", 
+    age: 23, 
+    address: "Sofia, Bulgaria",
+    id: 'asdasd'
+});
