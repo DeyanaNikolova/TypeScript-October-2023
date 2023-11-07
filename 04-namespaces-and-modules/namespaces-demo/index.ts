71325090a536016bc3cd650aa590fa25caef6b6b
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-11-07 15:02:14 +0000

    Export logic from namespace

--- 04-namespaces-and-modules/namespaces-demo/index.ts
+++ 04-namespaces-and-modules/namespaces-demo/index.ts
@@ -14,7 +14,7 @@ namespace PersonUtils {
   }
 }
 
-// const peshoPerson = new PersonUtils.Person('Pesho');
+const peshoPerson = new PersonUtils.Person('Pesho');
 
 class Person {
     name: string;
@@ -25,3 +25,15 @@ class Person {
 
 const p1 = new Person('Ivan');
 const p2 = new PersonUtils.Person('Stoyan');
+
+/// <reference path="animal.ts" />
+
+namespace AnimalGroup {
+ export class Dog extends Animal {
+    constructor(furColor: string, type: string) {
+      super(furColor, type);
+    }
+  }
+}
+
+const dog = new AnimalGroup.Dog('brown', 'Pincher');
