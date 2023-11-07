8ff29d88138c4b903e6f83cfd7a0ce25028133a3
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-11-07 18:14:00 +0000

    Update index.ts

--- 04-namespaces-and-modules/sceleton-demo/src/index.ts
+++ 04-namespaces-and-modules/sceleton-demo/src/index.ts
@@ -1,1 +1,18 @@
-console.log(123);
+import { Engine } from "./interfaces/Car";
+
+const engine1: Engine = {
+  horsPowers: 120,
+  make: "WV",
+};
+
+const engine2: Engine = {
+  horsPowers: 220,
+  make: "BMW",
+};
+
+console.log(engine1);
+console.log(engine2);
+
+for (let index = 0; index < 10; index++) {
+  console.log(index + 1);
+}
