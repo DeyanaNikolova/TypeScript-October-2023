cc525882fd2595f2651a5d2582fbf8ae59051b0b
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-11-07 16:49:10 +0000

    Update index.ts

--- 04-namespaces-and-modules/modules-demo/src/index.ts
+++ 04-namespaces-and-modules/modules-demo/src/index.ts
@@ -5,7 +5,10 @@ const person1: PersonDetails = { name: 'mitko', age: 23 };
 const person2: PersonDetailsWithoutAge = {name: 'ivan'};
 
 const personOneCapitalized = transformNameToUppercase(person1);
-const personTwoCapitalized = transformNameToUppercase(person2);
 console.log(person1);
 console.log(personOneCapitalized);
+
+const personTwoCapitalized = transformNameToUppercase(person2);
+console.log(person2);
+console.log(personTwoCapitalized);
 
