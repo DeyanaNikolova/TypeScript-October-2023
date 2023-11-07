d07bb56ce708b25595ce7e29e7ab49030bd91c70
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-11-07 16:46:38 +0000

    Capitalize person's name

--- 04-namespaces-and-modules/modules-demo/src/index.ts
+++ 04-namespaces-and-modules/modules-demo/src/index.ts
@@ -1,4 +1,11 @@
 import { PersonDetails, PersonDetailsWithoutAge } from "./interfaces/Person";
+import { transformNameToUppercase } from "./utils/person-util";
 
-const person: PersonDetails = { name: 'Mitko', age: 23 };
-const person2: PersonDetailsWithoutAge = {name: 'Ivan'};
+const person1: PersonDetails = { name: 'mitko', age: 23 };
+const person2: PersonDetailsWithoutAge = {name: 'ivan'};
+
+const personOneCapitalized = transformNameToUppercase(person1);
+const personTwoCapitalized = transformNameToUppercase(person2);
+console.log(person1);
+console.log(personOneCapitalized);
+
