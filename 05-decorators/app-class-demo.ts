25c6f6c26d880a671ce13fb4b79c35019c82ea1e
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-11-08 17:45:08 +0000

    Add class decorators example

---
+++ 05-decorators/app-class-demo.ts
@@ -0,0 +1,50 @@
+@withEmploymentDateOnPrototipe
+@withEmploymentDate
+class Meneger {
+  task: string = 'Simple task';
+  project: string = 'Simple project';
+
+  constructor() {
+    // console.log("Manager initialized!");
+  }
+}
+
+const menager = new Meneger();
+console.log(menager);
+// console.log(menager.constructor.prototype);
+
+function withEmploymentDateOnPrototipe(
+  value: Function,
+  context: ClassDecoratorContext
+) {
+  value.prototype.employmentDateOnPrototype = new Date().toISOString();
+}
+
+type Args = { new (...args: any[]): {}};
+function withEmploymentDate<T extends Args>(
+  baseClass: T,
+  context: ClassDecoratorContext
+    ) {
+        return class extends baseClass {
+            employmentDate = new Date().toISOString();
+
+            constructor(... args: any[]) {
+                super(... args);
+                console.log('Adding employment date to ' + baseClass.name);    
+            }
+        }
+}
+
+
+
+function printDecoratorData(
+  value: Function,
+  context: ClassDecoratorContext
+) {
+  console.log({ value });
+// console.log({ context });
+
+  context.addInitializer(() => {
+    // console.log("Class initialized: " + context.name);
+  });
+}
