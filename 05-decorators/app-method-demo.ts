f8f9050123f7ef61442993194da8f186cb4f9311
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-11-08 19:37:02 +0000

    Add method decorator example

---
+++ 05-decorators/app-method-demo.ts
@@ -0,0 +1,47 @@
+class Project {
+  budget: number = 900;
+
+  @withBudget(10)
+  writeTest() {
+    console.log("Tests are importanat!");
+  }
+
+  @withBudget(500)
+  fixBugInProduction() {
+    console.log("Fixing bugs in production is expensive!");
+  }
+}
+
+const project = new Project();
+project.writeTest();
+project.fixBugInProduction();
+project.fixBugInProduction();
+project.writeTest();
+project.writeTest();
+project.writeTest();
+console.log(project.budget);
+
+
+type WithBudget = {
+  budget: number;
+};
+
+function withBudget(actionBudget: number) {
+  return function <T extends WithBudget>(
+    target: Function,
+    context: ClassMethodDecoratorContext<T>
+  ) {
+    return function (...args: any) {
+        const instance = this as T; // T -> our decorated class
+
+      if(instance.budget > actionBudget){
+        instance.budget -= actionBudget;
+        target.apply(instance, args); // call our method for instance with args
+      }else{
+        console.log(`Insoficient budget for ${context.name.toString()}. Required ${actionBudget}, available ${instance.budget}!`);   
+      }
+    };
+  };
+}
+
+
