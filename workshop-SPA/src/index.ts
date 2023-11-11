1cdd92ff4e75fe15d3e278c88c18384716675503
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-11-11 18:05:22 +0000

    Add skeleton

---
+++ workshop-SPA/src/index.ts
@@ -0,0 +1,24 @@
+import { Engine } from "./interfaces/Car";
+import { capitalizeString } from "./utils/string-transformer";
+
+const engine1: Engine = {
+  horsPowers: 120,
+  make: "WV",
+};
+
+const engine2: Engine = {
+  horsPowers: 220,
+  make: "BMW",
+};
+
+const person = {};
+
+console.log(person);
+
+console.log(engine1);
+console.log(engine2);
+
+
+const randonVariable = capitalizeString('zdfsdsgg');
+console.log(randonVariable);
+
