bc4f32e3032a4c7c1b092c7d2af7ca8e439a860c
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-11-15 19:41:42 +0000

    Add solution to problem 9

--- lab-type-system/src/index.ts
+++ lab-type-system/src/index.ts
@@ -1,13 +1,13 @@
 //  01 - Hello World
 // function greeting(): void {
-//     console.log('Hello World!');   
+//     console.log('Hello World!');
 // }
 // greeting();
 
 //  02 - Numbers 1 to 10
 // function printNumbers(): void {
 //     for (let i = 1; i < 10; i++) {
-//         console.log(`${i}\n`); 
+//         console.log(`${i}\n`);
 //     }
 // }
 // printNumbers();
@@ -15,7 +15,7 @@
 //  03 - Calc rectangle area
 // function calcRectangleArea(length: number, width: number): void {
 //     const rectArea: number = length * width;
-//     console.log(rectArea);   
+//     console.log(rectArea);
 //  }
 // calcRectangleArea(5, 7);
 // calcRectangleArea(6, 8);
@@ -49,31 +49,63 @@
 //     numbersArray.push(arg3);
 
 //     let largest = numbersArray[0];
-  
+
 //     for (let i = 0; i < numbersArray.length; i++) {
-       
+
 //         if(numbersArray[i] > largest){
 //             largest = numbersArray[i]
 //         }
 //     }
-//     console.log(`The largest number is ${largest}`); 
+//     console.log(`The largest number is ${largest}`);
 // }
 // getLargestNumber(5, -3, 16);
 // getLargestNumber(-3, -5, -22.5);
 
 //  08 Sun of numbers n..m
-function calc(n: string, m: string): void {
-    const mAsNumber = Number(m);
-    const array: number[] = [];
+// function calc(n: string, m: string): void {
+//     const mAsNumber = Number(m);
+//     const array: number[] = [];
 
-    const initialValue: number = 0;
-   
-    for (let i = Number(n); i <= mAsNumber; i++) { 
-       array.push(i);
-    }
-    
-     let sum = array.reduce((previousVal, currentVal) => previousVal + currentVal, initialValue);
-     console.log(sum); 
+//     const initialValue: number = 0;
+
+//     for (let i = Number(n); i <= mAsNumber; i++) {
+//        array.push(i);
+//     }
+
+//      let sum = array.reduce((previousVal, currentVal) => previousVal + currentVal, initialValue);
+//      console.log(sum);
+// }
+// calc('1', '5');
+// calc('-8', '20');
+
+//  09 - Day fo week
+function dayOfWeek(weekDay: string): void {
+  switch (weekDay) {
+    case "Monday":
+      console.log(1);
+      break;
+    case "Tuesday":
+      console.log(2);
+      break;
+    case "Wednesday":
+      console.log(3);
+      break;
+    case "Thursday":
+      console.log(4);
+      break;
+    case "Friday":
+      console.log(5);
+      break;
+    case "Saturday":
+      console.log(6);
+      break;
+    case "Sunday":
+      console.log(7);
+      break;
+    default:
+      console.log("error");
+      break;
+  }
 }
-calc('1', '5');
-calc('-8', '20');
+dayOfWeek('Monday');
+dayOfWeek('Invalid');
