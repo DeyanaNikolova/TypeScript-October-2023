789dc89ad44c9a3ef4f990bc43ef1357d3d4fa70
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-11-16 09:30:56 +0000

    Add second solution for calculator

--- exam-prep/src/index.ts
+++ exam-prep/src/index.ts
@@ -36,3 +36,23 @@ console.log(calculator(25.5, '-', 3));
 console.log(calculator(9, '/', 2));
 console.log(calculator(7, '*', 5));
 
+// Probem 1 function
+function calculator1(a: number, operator: string, b: number): void {
+    
+    if(operator === '+'){
+      console.log(a + b);
+    }else if(operator === '-'){
+      console.log(a - b);
+    } else if(operator === '*'){
+      console.log(a * b);
+    } else{
+      console.log(a / b);
+    }
+     
+  }
+  
+  calculator1(5, '+', 10);
+  calculator1(25.5, '-', 3);
+  calculator1(7, '*', 5);
+  calculator1(9, '/', 2);
+
