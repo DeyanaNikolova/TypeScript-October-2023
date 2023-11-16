620e15d0821be0df8c0921d97330ec99069dcad6
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-11-16 13:07:34 +0000

    Add interfaces

--- lab-generics/src/index.ts
+++ lab-generics/src/index.ts
@@ -19,24 +19,40 @@
 // console.log(box3.toString());
 
 // 02 - Generic Compare Elements
-class CompareElements<T>{
-    genericType: T[];
-    constructor(genericType: T[]) {
-    this.genericType = genericType;   
-    }
-    compare(arg: any){
-      let comparator: number = 0;
-      for (let i = 0; i < this.genericType.length; i++) {
-        if(arg === this.genericType[i]){
-            comparator++;  
-        }
-      }
-      return comparator;
-    }
+// class CompareElements<T>{
+//     genericType: T[];
+//     constructor(genericType: T[]) {
+//     this.genericType = genericType;   
+//     }
+//     compare(arg: any){
+//       let comparator: number = 0;
+//       for (let i = 0; i < this.genericType.length; i++) {
+//         if(arg === this.genericType[i]){
+//             comparator++;  
+//         }
+//       }
+//       return comparator;
+//     }
+// }
+
+// const c = new CompareElements(['a', 'b', 'ab', 'abc', 'cba', 'b']);
+// console.log(c.compare('b'));
+
+// const c1 = new CompareElements([1, 2, 3, 4, 5, 1, 1]);
+// console.log(c1.compare(1));
+
+//  03 - Car Dealership
+interface Dealership<T>{
+    dealershipName: T;
+    soldCar: number;
 }
 
-const c = new CompareElements(['a', 'b', 'ab', 'abc', 'cba', 'b']);
-console.log(c.compare('b'));
+interface Actions<T, V>{
+dealerId: T;
+model: V;
+}
 
-const c1 = new CompareElements([1, 2, 3, 4, 5, 1, 1]);
-console.log(c1.compare(1));
+class CarDealership {
+modelSold: [] = [];
+
+}
